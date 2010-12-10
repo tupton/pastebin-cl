8ff3cdda9e0027c41416b285f2822781a0ba4b5e
tupton <thomasupton@gmail.com>
2010-12-09 22:08:30 -0600

    Don't include the POST param `paste_code` in the text that's copied to the clipboard.

--- pastebin.sh
+++ pastebin.sh
@@ -39,12 +39,12 @@ shift $((OPTIND - 1))
 PASTEBIN_FILE=${PASTEBIN_FILE:-/dev/stdin}
 PASTEBIN_COPY=${PASTEBIN_COPY:-0}
 
-PASTEBIN_TO_PASTE="paste_code=`cat $PASTEBIN_FILE`"
+PASTEBIN_TO_PASTE=`cat $PASTEBIN_FILE`
 
 if [ $PASTEBIN_COPY = 1 ]; then
     echo "$PASTEBIN_TO_PASTE" | pbcopy
     sleep .5
 fi
 
-curl -s -X POST -d "$PASTEBIN_TO_PASTE" $PASTEBIN_API | pbcopy
+curl -s -X POST -d "paste_code=$PASTEBIN_TO_PASTE" $PASTEBIN_API | pbcopy
 
